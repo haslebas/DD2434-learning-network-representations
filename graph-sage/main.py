dcb01ed695fb3938645395d58db7cad7116af450
Luca Marini <lucamarini1998@gmail.com>
2021-12-30 12:37:28 +0100

    add pubmed dataset, compute f1 score on node classification

--- graph-sage/main.py
+++ graph-sage/main.py
@@ -2,6 +2,7 @@
 # inspired by https://colab.research.google.com/github/stellargraph/stellargraph/blob/master/demos/embeddings/
 # graphsage-unsupervised-sampler-embeddings.ipynb
 
+import tensorflow as tf
 import argparse
 import random
 from stellargraph.mapper import GraphSAGELinkGenerator
@@ -11,7 +12,7 @@ from sklearn.model_selection import train_test_split
 
 from tensorflow import keras
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
@@ -19,13 +20,14 @@ from stellargraph.mapper import GraphSAGENodeGenerator
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow_addons as tfa
 
 from dataset_utils import *
 
 
 def main(args):
     # load dataset
-    cora_dataset, G, node_subjects, nodes = get_dataset(args.dataset)
+    cora_dataset, G, labels, nodes = get_dataset(args.dataset)
 
     unsupervised_samples = UnsupervisedSampler(
         G, nodes=nodes, length=args.length, number_of_walks=args.number_of_walks
@@ -34,12 +36,12 @@ def main(args):
     generator = GraphSAGELinkGenerator(G, args.batch_size, args.num_samples)
     train_gen = generator.flow(unsupervised_samples)
 
-    graphsage = GraphSAGE(
+    graphsage_model = GraphSAGE(
         layer_sizes=args.layer_sizes, generator=generator, bias=True, dropout=args.dropout, normalize=args.normalization
     )
 
     # Build the model and expose input and output sockets of graphsage, for node pair inputs:
-    x_inp, x_out = graphsage.in_out_tensors()
+    x_inp, x_out = graphsage_model.in_out_tensors()
 
     prediction = link_classification(
         output_dim=1, output_act="sigmoid", edge_embedding_method="ip"
@@ -47,10 +49,13 @@ def main(args):
 
     model = keras.Model(inputs=x_inp, outputs=prediction)
 
+    num_classes = len(set(labels))
     model.compile(
         optimizer=keras.optimizers.Adam(learning_rate=args.learning_rate),
         loss=keras.losses.binary_crossentropy,
-        metrics=[keras.metrics.binary_accuracy],
+        metrics=[keras.metrics.binary_accuracy]#,
+                 #tfa.metrics.F1Score(num_classes=num_classes, average='micro', name='f1_micro'),
+                 #tfa.metrics.F1Score(num_classes=num_classes, average='macro', name='f1_macro')],
     )
 
     history = model.fit(
@@ -68,22 +73,44 @@ def main(args):
 
 
 
+    '''
+    from sklearn import preprocessing, feature_extraction, model_selection
+
+    labels_sampled = labels.sample(frac=0.8, replace=False, random_state=101)
+    train_labels, test_labels = model_selection.train_test_split(
+        labels_sampled,
+        train_size=0.05,
+        test_size=None,
+        stratify=labels_sampled,
+        random_state=42,
+    )
+
+    target_encoding = preprocessing.LabelBinarizer()
+    train_targets = target_encoding.fit_transform(train_labels)
+    test_targets = target_encoding.transform(test_labels)
+    test_gen = generator.flow(test_labels.index, test_targets)
+
+    test_metrics = model.evaluate(test_gen)
+    print("\nTest Set Metrics:")
+    for name, val in zip(model.metrics_names, test_metrics):
+        print("\t{}: {:0.4f}".format(name, val))
+    '''
+
+
     x_inp_src = x_inp[0::2]
     x_out_src = x_out[0]
     embedding_model = keras.Model(inputs=x_inp_src, outputs=x_out_src)
 
-    node_ids = node_subjects.index
+    node_ids = labels.index
     node_gen = GraphSAGENodeGenerator(G, args.batch_size, args.num_samples).flow(node_ids)
 
     node_embeddings = embedding_model.predict(node_gen, workers=4, verbose=1)
 
-    node_subject = node_subjects.astype("category").cat.codes
+    node_subject = labels.astype("category").cat.codes
 
 
-
-
-
-    #'''
+    '''
+    # plot of the node embeddings
     X = node_embeddings
     if X.shape[1] > 2:
         transform = TSNE  # PCA
@@ -95,7 +122,7 @@ def main(args):
         emb_transformed = pd.DataFrame(X, index=node_ids)
         emb_transformed = emb_transformed.rename(columns={"0": 0, "1": 1})
         emb_transformed["label"] = node_subject
-    
+
     alpha = 0.7
 
     fig, ax = plt.subplots(figsize=(7, 7))
@@ -108,52 +135,92 @@ def main(args):
     )
     ax.set(aspect="equal", xlabel="$X_1$", ylabel="$X_2$")
     plt.title(
-        "{} visualization of GraphSAGE embeddings for cora dataset".format(transform.__name__)
+        "{} visualization of GraphSAGE embeddings".format(transform.__name__)
     )
     plt.show()
-    #'''
-
-
-
-
-
-
-    # node classification
-    '''
-    # X will hold the 50 input features (node embeddings)
-    X = node_embeddings
-    # y holds the corresponding target values
-    y = np.array(node_subject)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, train_size=0.05, test_size=None, stratify=y
-    )
-
-    clf = LogisticRegression(verbose=0, solver="lbfgs", multi_class="auto")
-    clf.fit(X_train, y_train)
-
-    y_pred = clf.predict(X_test)
-
-    accuracy_score(y_test, y_pred)
-
-    print(pd.Series(y_pred).value_counts())
-
-    print(pd.Series(y).value_counts())
     '''
 
-    # link prediction
+
+
+
+
+    # Downstream task
+    if args.task == "node_classification":
+        # node classification
+        print("node classification")
+
+        # X will hold the 50 input features (node embeddings)
+        X = node_embeddings
+        # y holds the corresponding target values
+        y = np.array(node_subject)
+    
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, train_size=0.05, test_size=None, stratify=y
+        )
+    
+        clf = LogisticRegression(verbose=0, solver="lbfgs", multi_class="auto")
+        clf.fit(X_train, y_train)
+        '''
+        print(X_train.shape)
+        print(y_train.shape)
+        print(X_test.shape)
+        print(y_test.shape)
+
+        inputs = tf.keras.Input(shape=y_train.shape)
+        #outputs = tf.keras.Output(shape=y_test.shape)
+        print(x_inp)
+        print('oooo')
+        print(x_inp_src)
+        model = keras.Model(inputs=inputs, outputs=x_out_src)
+
+        num_classes = len(set(labels))
+        model.compile(
+            optimizer=keras.optimizers.Adam(learning_rate=args.learning_rate),
+            loss=keras.losses.binary_crossentropy,
+            metrics=[keras.metrics.binary_accuracy,
+                     tfa.metrics.F1Score(num_classes=num_classes, average='micro', name='f1_micro'),
+                     tfa.metrics.F1Score(num_classes=num_classes, average='macro', name='f1_macro')],
+        )
+    
+        history = model.fit(
+            x=X_train,
+            y=y_train,
+            epochs=args.epochs,
+            verbose=1,
+            use_multiprocessing=False,
+            workers=4,
+            shuffle=True,
+        )
+        
+        y_pred = model.predict(X_test) 
+        '''
+
+        y_pred = clf.predict(X_test)
+
+        print('f1_micro: {}'.format(f1_score(y_test, y_pred, average='micro')))
+        print('f1_macro: {}'.format(f1_score(y_test, y_pred, average='macro')))
+    
+        print(pd.Series(y_pred).value_counts())
+    
+        print(pd.Series(y).value_counts())
+
+    else:
+        # link prediction
+        print("link prediction")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='graphsage - generation of node embeddings')
 
     # command-line arguments
+    parser.add_argument('-task', dest='task', type=str,
+                        help='downstream task', action='store', default="node_classification")
     parser.add_argument('-dataset', dest='dataset', type=str,
-                        help='chosen dataset', action='store', default="cora")
+                        help='chosen dataset', action='store', default="pubmed")
     parser.add_argument('--seed', dest='seed', type=int,
                         help='fix random seeds', action='store', default=1)
     parser.add_argument('-e', dest='epochs', type=int,
-                        help='number of epochs', action='store', default=4)
+                        help='number of epochs', action='store', default=1)
     parser.add_argument('-nw', dest='number_of_walks', type=int,
                         help='number of walks to take per node', action='store', default=1)
     parser.add_argument('-l', dest='length', type=int,
