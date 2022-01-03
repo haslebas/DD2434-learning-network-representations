f430c09fea78c341ea358508b93962c2f00344c6
Filippa Karrfelt <filippakarrfelt@n141-p253.eduroam.kth.se>
2022-01-03 15:20:14 +0100

    netMF changes

--- NetMF/run_netmf.sh
+++ NetMF/run_netmf.sh
@@ -1,7 +1,7 @@
 # To run the NetMF approximation implementation
 
 # Run approximation
-python main.py ../data/BlogCatalog-dataset/blog_catalog_graph.gpickle ../data/BlogCatalog-dataset/netmf_approx_embedding.npy --large
+python main.py ../data/BlogCatalog-dataset/blog_catalog_graph.gpickle ../data/BlogCatalog-dataset/blogcat_netmf_approx_embedding_128d.pkl --large
 
 # Run exact
-# python main.py ../data/BlogCatalog-dataset/blog_catalog_graph.gpickle ../data/BlogCatalog-dataset/netmf_embedding.npy
+# python main.py ../data/BlogCatalog-dataset/blog_catalog_graph.gpickle ../data/BlogCatalog-dataset/blogcat_netmf_exact_embedding_128d.npy
