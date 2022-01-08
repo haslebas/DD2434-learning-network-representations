677216d82fe7d53c77912a2263ee388308717fdc
manuelfraile <manu.fra.ro@gmail.com>
2022-01-08 01:44:36 +0100

    node2vec final v

--- node2vec/run_linkprediction_node2vec.sh
+++ node2vec/run_linkprediction_node2vec.sh
@@ -1,2 +1,2 @@
 # Node2Vec BlogCatalog embeddings
-python3 ../LinkPrediction/link_prediction.py ./emb/node2vec_model1.pkl ../data/BlogCatalog-dataset/blog_catalog_graph_lp_test_edges.pkl
+python3 ../LinkPrediction/link_prediction.py ./emb/node2vec_corabig_m1.pkl ../data/BlogCatalog-dataset/blog_catalog_graph_lp_test_edges.pkl --dataset_name BlogCatalog
--- node2vec/run_node2vec.sh
+++ node2vec/run_node2vec.sh
@@ -1,2 +1,12 @@
 # Run node2Vec on BlogCatalog
-python ./src/mainref.py --input ./graph/karate.edgelist --output ./emb/node2vec_model1
+python ./src/main.py --input ../data/BlogCatalog-dataset/blog_catalog_graph.gpickle --output ./emb/my_model --dataset BlogCatalog
+
+# Run node2Vec on BlogCatalog
+#python ./src/mainref.py --input ../data/BlogCatalog-dataset/blog_catalog_graph.gpickle --output ./emb/node2vec_model2
+
+# Run node2Vec on BlogCatalog
+#python ./src/mainref.py --input ./graph/karate.edgelist --output ./emb/pruebas
+
+# Run node2Vec on Cora Big
+#python ./src/mainref.py --input ../data/subelj_cora/cora_big_graph_dir.gpickle --output ./emb/node2vec_corabig_m2 --directed
+#python ./src/mainref.py --input ./graph/CoraBig.edgelist --output ./emb/node2vec_corabig_m2 --directed
